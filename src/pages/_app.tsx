f2323fe37d2e88bc4526ef433e3924f908b41e36
Ryosuke Sato <postscaperyosuke@gmail.com>
2023-03-03 23:02:42 +0900

    [add]基本のstyle設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/styles/globals.css'
+import 'ress'
+import { Noto_Sans_Javanese } from 'next/font/google'
 import type { AppProps } from 'next/app'
 
+const notoSansJapanese = Noto_Sans_Javanese({
+  weight: ['400','700'],
+  subsets: ['javanese'],
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <style jsx global>{`
+        html {
+          font-family: ${notoSansJapanese.style.fontFamily};
+        }
+      `}</style>
+  <Component {...pageProps} />
+  </>
+  )
 }
