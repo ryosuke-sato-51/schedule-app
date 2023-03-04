9d24767174a01bbf75ac688e80d56e97c5608bd1
Ryosuke Sato <postscaperyosuke@gmail.com>
2023-03-04 14:45:48 +0900

    [add]chakra UI追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,12 @@
 import '@/styles/globals.css'
 import 'ress'
-import { Noto_Sans_Javanese } from 'next/font/google'
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
-
-const notoSansJapanese = Noto_Sans_Javanese({
-  weight: ['400','700'],
-  subsets: ['javanese'],
-})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <>
-  <style jsx global>{`
-        html {
-          font-family: ${notoSansJapanese.style.fontFamily};
-        }
-      `}</style>
-  <Component {...pageProps} />
-  </>
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
   )
 }
